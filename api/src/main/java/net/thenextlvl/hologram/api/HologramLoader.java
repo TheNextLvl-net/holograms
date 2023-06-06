66e7055042caa024e369d527e56b5a7d0669e2ae
NonSwag <mrminecraft00@gmail.com>
2023-06-06 18:40:24 +0200

    updated java docs

--- api/src/main/java/net/thenextlvl/hologram/api/HologramLoader.java
+++ api/src/main/java/net/thenextlvl/hologram/api/HologramLoader.java
@@ -13,14 +13,12 @@ public interface HologramLoader {
      * Loads the hologram for the specified player
      *
      * @param hologram the hologram to load
-     * @param player the player to load the hologram for
-     *
+     * @param player   the player to load the hologram for
      * @throws IllegalArgumentException thrown if the hologram is
-     * {@link HologramLoader#isLoaded(Hologram, Player) already loaded} or not
-     * {@link HologramLoader#canSee(Player, Hologram) visible} to the player
-     *
-     * @throws NullPointerException thrown if the
-     * {@link Location#getWorld() world} of the hologram is null
+     *                                  {@link HologramLoader#isLoaded(Hologram, Player) already loaded} or not
+     *                                  {@link HologramLoader#canSee(Player, Hologram) visible} to the player
+     * @throws NullPointerException     thrown if the
+     *                                  {@link Location#getWorld() world} of the hologram is null
      */
     void load(Hologram hologram, Player player) throws IllegalArgumentException, NullPointerException;
 
@@ -37,10 +35,10 @@ public interface HologramLoader {
      * Unloads the hologram for the specified player
      *
      * @param hologram the hologram to unload
-     * @param player the player to unload the hologram for
-     *
+     * @param player   the player to unload the hologram for
      * @throws IllegalArgumentException thrown if the hologram is not
-     * {@link HologramLoader#isLoaded(Hologram, Player) loaded}
+     *                                  {@link HologramLoader#isLoaded(Hologram, Player) loaded}
+     * @see HologramLoader#unload(Hologram)
      */
     void unload(Hologram hologram, Player player) throws IllegalArgumentException;
 
@@ -59,14 +57,13 @@ public interface HologramLoader {
      * Updates the hologram for the specified player
      *
      * @param hologram the hologram to update
-     * @param player the player to update the hologram for
-     *
+     * @param player   the player to update the hologram for
      * @throws IllegalArgumentException thrown if the hologram is
-     * {@link HologramLoader#isLoaded(Hologram, Player) not loaded} or not
-     * {@link HologramLoader#canSee(Player, Hologram) visible} to the player
-     *
-     * @throws NullPointerException thrown if the
-     * {@link Location#getWorld() world} of the hologram is null
+     *                                  {@link HologramLoader#isLoaded(Hologram, Player) not loaded} or not
+     *                                  {@link HologramLoader#canSee(Player, Hologram) visible} to the player
+     * @throws NullPointerException     thrown if the
+     *                                  {@link Location#getWorld() world} of the hologram is null
+     * @see HologramLoader#update(Hologram)
      */
     void update(Hologram hologram, Player player) throws IllegalArgumentException, NullPointerException;
 
@@ -74,15 +71,16 @@ public interface HologramLoader {
      * Checks if the hologram is loaded for the player
      *
      * @param hologram the hologram
-     * @param player the player
+     * @param player   the player
      * @return true if the hologram is loaded for the player
+     * @see HologramLoader#getViewers(Hologram)
      */
     boolean isLoaded(Hologram hologram, Player player);
 
     /**
      * Checks if the player could possibly see the hologram
      *
-     * @param player the player
+     * @param player   the player
      * @param hologram the hologram
      * @return true if the hologram can be seen by the player
      */

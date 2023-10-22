66a25d5df6b18e62a02748a819fd169752e9ddeb
NonSwag <mrminecraft00@gmail.com>
2023-10-22 16:25:39 +0200

    updated plugin api version to 1.20.2

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.28")
     compileOnly("net.thenextlvl.core:annotations:2.0.0")
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 
     implementation(project(":api"))
     implementation(project(":v1_19_4", "reobf"))

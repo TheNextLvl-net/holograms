b642034390475db3bda7cbf0b998a44b8b9a8e98
david <mrminecraft00@gmail.com>
2024-03-11 20:01:32 +0100

    updated java version to 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 group = "net.thenextlvl.holograms"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
 }

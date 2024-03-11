b6189a817209bd4cee92d31abbceecdda389896c
david <mrminecraft00@gmail.com>
2024-03-11 20:01:42 +0100

    code cleanup

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -27,21 +27,17 @@ dependencies {
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-        repositories {
-            maven {
-                val channel = if ((version as String).contains("-pre")) "snapshots" else "releases"
-                url = uri("https://repo.thenextlvl.net/$channel")
-                credentials {
-                    if (extra.has("RELEASES_USER"))
-                        username = extra["RELEASES_USER"].toString()
-                    if (extra.has("RELEASES_PASSWORD"))
-                        password = extra["RELEASES_PASSWORD"].toString()
-                }
-            }
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+    repositories.maven {
+        val channel = if ((version as String).contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$channel")
+        credentials {
+            if (extra.has("RELEASES_USER"))
+                username = extra["RELEASES_USER"].toString()
+            if (extra.has("RELEASES_PASSWORD"))
+                password = extra["RELEASES_PASSWORD"].toString()
         }
     }
 }

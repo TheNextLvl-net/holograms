7f95288beb34e3db66cebcbd6b9a6a2d156ff6ec
david <mrminecraft00@gmail.com>
2024-05-02 15:30:09 +0200

    removed unsupported modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,3 @@
 rootProject.name = "holograms"
 include("api")
 include("plugin")
-include("v1_19_4")
-include("v1_20_1")
-include("v1_20_2")
-include("v1_20_4")
--- v1_20_4/src/main/java/net/thenextlvl/hologram/v1_20_R3/package-info.java
+++
@@ -1,8 +0,0 @@
-@TypesAreNotNullByDefault
-@MethodsReturnNotNullByDefault
-@ParametersAreNotNullByDefault
-package net.thenextlvl.hologram.v1_20_R3;
-
-import core.annotation.MethodsReturnNotNullByDefault;
-import core.annotation.ParametersAreNotNullByDefault;
-import core.annotation.TypesAreNotNullByDefault;

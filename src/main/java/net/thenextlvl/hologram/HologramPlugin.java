3d44db7ab08760c4ffddf9c40a010bf779ab661c
david <github@nonswag.dev>
2025-05-10 18:47:54 +0200

    Replaced `Bukkit` usage with `getServer` methods
    Improved code consistency by using `getServer()` for service and plugin manager access instead of `Bukkit`.

--- src/main/java/net/thenextlvl/hologram/HologramPlugin.java
+++ src/main/java/net/thenextlvl/hologram/HologramPlugin.java
@@ -4,7 +4,6 @@ import net.thenextlvl.hologram.api.HologramProvider;
 import net.thenextlvl.hologram.implementation.CraftHologramProvider;
 import net.thenextlvl.hologram.listener.HologramListener;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
@@ -15,12 +14,12 @@ public class HologramPlugin extends JavaPlugin {
     private final Metrics metrics = new Metrics(this, 20033);
 
     public HologramPlugin() {
-        Bukkit.getServicesManager().register(HologramProvider.class, provider, this, ServicePriority.Normal);
+        getServer().getServicesManager().register(HologramProvider.class, provider, this, ServicePriority.Normal);
     }
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new HologramListener(provider), this);
+        getServer().getPluginManager().registerEvents(new HologramListener(provider), this);
     }
 
     @Override

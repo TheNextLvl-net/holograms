01c418d1f7b61ae54cfa8f2b566f4e6d677a6fbd
david <github@nonswag.dev>
2025-09-20 13:10:05 +0200

    Add CHANGELOG environment variable to plugin publication
    Set the changelog property in the plugin publication to use the CHANGELOG environment variable.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
     publications.register("plugin") {
         id.set("Holograms")
         version.set(versionString)
+        changelog = System.getenv("CHANGELOG")
         channel.set(if (isRelease) "Release" else "Snapshot")
         apiKey.set(System.getenv("HANGAR_API_TOKEN"))
         platforms.register(Platforms.PAPER) {

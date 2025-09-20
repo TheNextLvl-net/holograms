2425c36b564fe1e17d9035d477251b19e951fbf9
david <github@nonswag.dev>
2025-09-20 13:21:32 +0200

    Add module-info.java for module declarations
    Define module and its dependencies for the holograms project.

---
+++ api/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module net.thenextlvl.holograms {
+    exports net.thenextlvl.hologram.line;
+    exports net.thenextlvl.hologram;
+
+    requires net.kyori.adventure;
+    requires net.kyori.examination.api;
+    requires org.bukkit;
+    requires org.joml;
+
+    requires static org.jetbrains.annotations;
+    requires static org.jspecify;
+}

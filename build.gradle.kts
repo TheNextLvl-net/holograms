4a3b35e18a87d0bcb063f0bc8d9d2b8d0a0e0031
david <github@nonswag.dev>
2025-09-20 13:09:58 +0200

    Add Modrinth plugin configuration to build script
    Integrate the Modrinth Minotaur plugin for project publishing,
    including environment variable setup for token and changelog.
    Prepare for project ID assignment and file upload configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     id("java")
     id("com.gradleup.shadow") version "9.1.0"
+    id("com.modrinth.minotaur") version "2.+"
     id("de.eldoria.plugin-yml.paper") version "0.8.0"
     id("io.papermc.hangar-publish-plugin") version "0.1.3"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
@@ -70,4 +71,15 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
             platformVersions.set(versions)
         }
     }
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("") // todo: create project
+    changelog = System.getenv("CHANGELOG")
+    versionType = if (isRelease) "release" else "beta"
+    uploadFile.set(tasks.shadowJar)
+    gameVersions.set(versions)
+    syncBodyFrom.set(rootProject.file("README.md").readText())
+    loaders.add("paper")
 }

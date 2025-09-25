bd883af0d41bfd6c0635e23ae62c6e90d140df2d
david <mail@nonswag.dev>
2025-09-25 20:38:07 +0200

    Downgrade bstats-bukkit dependency from 3.1.1-SNAPSHOT to 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
     implementation("net.thenextlvl.core:i18n:3.2.2")
     implementation("net.thenextlvl:nbt:3.0.1")
-    implementation("org.bstats:bstats-bukkit:3.1.1-SNAPSHOT")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation(project(":api"))
 }
 

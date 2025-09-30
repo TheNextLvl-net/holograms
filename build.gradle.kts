e24cc75715ef2050bdce64cd73ddad25b5da2aa8
david <mail@nonswag.dev>
2025-09-30 19:58:14 +0200

    Update dependencies to 1.21.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.9-R0.1-SNAPSHOT")
     implementation("net.thenextlvl.core:i18n:3.2.2")
     implementation("net.thenextlvl:nbt:3.0.1")
     implementation("org.bstats:bstats-bukkit:3.1.0")

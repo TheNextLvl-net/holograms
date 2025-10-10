00c8709985a84648fac68e6968b2eb26e789f9e8
david <mail@nonswag.dev>
2025-10-10 21:25:19 +0200

    Update dependencies to 1.21.10

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
 }
 
 tasks.withType<Javadoc>().configureEach {

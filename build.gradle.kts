4f423cd5eb12666f89be958355c9ef8a22947211
david <mail@nonswag.dev>
2026-01-05 23:38:04 +0100

    Updated NBT dependency to 4.3.1 in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
     implementation("dev.faststats.metrics:bukkit:0.8.1")
     implementation("net.thenextlvl:i18n:1.1.0")
-    implementation("net.thenextlvl:nbt:4.3.0")
+    implementation("net.thenextlvl:nbt:4.3.1")
     implementation("net.thenextlvl:static-binder:0.1.3")
     implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation(project(":api"))

3e860630e26caf4f1ce8ac92ef920a2f4647fc42
david <mail@nonswag.dev>
2026-01-13 19:25:40 +0100

    Update billboard command to send feedback message

--- src/main/java/net/thenextlvl/hologram/commands/line/edit/HologramLineEditBillboardCommand.java
+++ src/main/java/net/thenextlvl/hologram/commands/line/edit/HologramLineEditBillboardCommand.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.hologram.Hologram;
 import net.thenextlvl.hologram.HologramPlugin;
 import net.thenextlvl.hologram.commands.arguments.EnumArgumentType;
@@ -26,11 +28,28 @@ final class HologramLineEditBillboardCommand extends SimpleCommand {
 
     @Override
     public int run(CommandContext<CommandSourceStack> context) {
+        var sender = context.getSource().getSender();
         var hologram = context.getArgument("hologram", Hologram.class);
-        var line = hologram.getLine(context.getArgument("line", int.class) - 1, DisplayHologramLine.class);
+        var line = context.getArgument("line", int.class);
         var billboard = context.getArgument("billboard", Billboard.class);
-        line.ifPresent(displayLine -> displayLine.setBillboard(billboard));
-        // todo: send message
+
+        var message = hologram.getLine(line - 1, DisplayHologramLine.class).map(textLine -> {
+            if (textLine.getBillboard() == billboard) return "nothing.changed";
+            textLine.setBillboard(billboard);
+            return "hologram.billboard";
+        }).orElse("hologram.type.display");
+
+        var billboardName = plugin.bundle().component(switch (billboard) {
+            case FIXED -> "billboard.fixed";
+            case VERTICAL -> "billboard.vertical";
+            case HORIZONTAL -> "billboard.horizontal";
+            case CENTER -> "billboard.center";
+        }, sender);
+
+        plugin.bundle().sendMessage(sender, message,
+                Placeholder.parsed("hologram", hologram.getName()),
+                Placeholder.component("billboard", billboardName),
+                Formatter.number("line", line));
         return SINGLE_SUCCESS;
     }
 }

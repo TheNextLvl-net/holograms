03fe19d5abd6a42783439394fff930af4e9dea92
david <mail@nonswag.dev>
2026-01-13 19:29:05 +0100

    Improve hologram scale command message handling

--- src/main/java/net/thenextlvl/hologram/commands/line/edit/HologramLineEditScaleCommand.java
+++ src/main/java/net/thenextlvl/hologram/commands/line/edit/HologramLineEditScaleCommand.java
@@ -5,6 +5,8 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.hologram.Hologram;
 import net.thenextlvl.hologram.HologramPlugin;
 import net.thenextlvl.hologram.commands.brigadier.SimpleCommand;
@@ -33,22 +35,28 @@ final class HologramLineEditScaleCommand extends SimpleCommand {
     @Override
     public int run(CommandContext<CommandSourceStack> context) {
         var hologram = context.getArgument("hologram", Hologram.class);
+        var lineNumber = context.getArgument("line", int.class);
         var scale = tryGetArgument(context, "scale", float.class).map(Vector3f::new).orElseGet(() -> {
             var x = context.getArgument("x", float.class);
             var y = context.getArgument("y", float.class);
             var z = context.getArgument("z", float.class);
             return new Vector3f(x, y, z);
         });
-        
-        var line = hologram.getLine(context.getArgument("line", int.class) - 1).orElse(null);
-        if (line instanceof DisplayHologramLine<?, ?> displayLine) {
-            var transformation = displayLine.getTransformation();
-            transformation.getScale().set(scale);
-            displayLine.setTransformation(transformation);
-        } else if (line instanceof EntityHologramLine<?> entityLine) {
-            entityLine.setScale(scale.y());
-        }
-        // todo: send message
+
+        var message = hologram.getLine(lineNumber - 1).map(line -> {
+            if (line instanceof DisplayHologramLine<?, ?> displayLine) {
+                var transformation = displayLine.getTransformation();
+                transformation.getScale().set(scale);
+                displayLine.setTransformation(transformation);
+            } else if (line instanceof EntityHologramLine<?> entityLine) {
+                entityLine.setScale(scale.y());
+            } else return "hologram.type.display";
+            return "hologram.scale";
+        }).orElse("hologram.line.invalid");
+
+        plugin.bundle().sendMessage(context.getSource().getSender(), message,
+                Placeholder.parsed("hologram", hologram.getName()),
+                Formatter.number("line", lineNumber));
         return SINGLE_SUCCESS;
     }
 }

506261b3930b2fc5ecac65f7ce92ffb406aa450b
david <mail@nonswag.dev>
2026-01-27 20:58:36 +0100

    Add module exports and dependencies for action and kyori

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -2,12 +2,16 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 module net.thenextlvl.holograms {
+    exports net.thenextlvl.hologram.action;
     exports net.thenextlvl.hologram.event;
     exports net.thenextlvl.hologram.image;
     exports net.thenextlvl.hologram.line;
     exports net.thenextlvl.hologram;
 
+    requires com.google.common;
     requires java.desktop;
+    requires net.kyori.adventure.key;
+    requires net.kyori.adventure.text.minimessage;
     requires net.kyori.adventure;
     requires net.kyori.examination.api;
     requires net.thenextlvl.binder;

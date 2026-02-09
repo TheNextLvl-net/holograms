41aa537b3da7eeb89df48b86e5efb80695e92995
david <mail@nonswag.dev>
2026-02-09 18:53:42 +0100

    Add paged hologram page change/add/remove events

---
+++ api/src/main/java/net/thenextlvl/hologram/event/HologramPageAddEvent.java
@@ -0,0 +1,58 @@
+package net.thenextlvl.hologram.event;
+
+import net.thenextlvl.hologram.Hologram;
+import net.thenextlvl.hologram.line.PagedHologramLine;
+import net.thenextlvl.hologram.line.StaticHologramLine;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
+
+/**
+ * Called when a page is added to a {@link PagedHologramLine}.
+ *
+ * @since 0.8.0
+ */
+public final class HologramPageAddEvent extends HologramEvent {
+    private static final HandlerList handlers = new HandlerList();
+    private final PagedHologramLine line;
+    private final StaticHologramLine page;
+
+    @ApiStatus.Internal
+    public HologramPageAddEvent(final Hologram hologram, final PagedHologramLine line, final StaticHologramLine page) {
+        super(hologram);
+        this.line = line;
+        this.page = page;
+    }
+
+    /**
+     * Returns the paged line the page was added to.
+     *
+     * @return the paged hologram line
+     * @since 0.8.0
+     */
+    @Contract(pure = true)
+    public PagedHologramLine getLine() {
+        return line;
+    }
+
+    /**
+     * Returns the page that was added.
+     *
+     * @return the added page
+     * @since 0.8.0
+     */
+    @Contract(pure = true)
+    public StaticHologramLine getPage() {
+        return page;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @Contract(pure = true)
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}

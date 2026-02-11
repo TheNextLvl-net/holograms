af8090cff9945b54b552df3bd90ec0408bfce937
david <mail@nonswag.dev>
2026-02-11 19:48:06 +0100

    Inject plugin into EmptyEconomyProvider

--- src/main/java/net/thenextlvl/hologram/economy/EmptyEconomyProvider.java
+++ src/main/java/net/thenextlvl/hologram/economy/EmptyEconomyProvider.java
@@ -1,12 +1,20 @@
 package net.thenextlvl.hologram.economy;
 
+import net.thenextlvl.hologram.HologramPlugin;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public final class EmptyEconomyProvider implements EconomyProvider {
+    private final HologramPlugin plugin;
+    
+    public EmptyEconomyProvider(final HologramPlugin plugin) {
+        this.plugin = plugin;
+    }
+    
     @Override
     public boolean withdraw(final Player player, final double amount) {
+        if (amount == 0) return true;
         return false;
     }
 }

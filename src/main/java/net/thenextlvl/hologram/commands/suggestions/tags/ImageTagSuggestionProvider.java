b20eab7c26ca1d3df7d7178a1486479c045db4be
david <mail@nonswag.dev>
2026-02-20 19:00:05 +0100

    Improve image tag suggestion spacing and offset logic

--- src/main/java/net/thenextlvl/hologram/commands/suggestions/tags/ImageTagSuggestionProvider.java
+++ src/main/java/net/thenextlvl/hologram/commands/suggestions/tags/ImageTagSuggestionProvider.java
@@ -13,24 +13,24 @@ public final class ImageTagSuggestionProvider<S> implements SuggestionProvider<S
     @Override
     public CompletableFuture<Suggestions> getSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
         final var remaining = builder.getRemaining();
-        final var trimmed = remaining.trim();
-        if (trimmed.isEmpty()) {
+        final var stripped = remaining.strip();
+        if ("https://".startsWith(stripped)) {
             builder.suggest("https://");
-        } else if (trimmed.endsWith(",")) {
+        } else if (stripped.endsWith(",")) {
             final var offset = builder.createOffset(builder.getStart() + remaining.lastIndexOf(',') + 1);
             for (var i = 1; i <= 9; i++) offset.suggest(i);
             builder.add(offset);
-        } else if (trimmed.contains(",") && Character.isDigit(trimmed.charAt(trimmed.length() - 1))) {
+        } else if (stripped.contains(",") && Character.isDigit(stripped.charAt(stripped.length() - 1))) {
             final var beginIndex = remaining.lastIndexOf(',') + 1;
             final var offset = builder.createOffset(builder.getStart() + beginIndex);
-            final var number = remaining.substring(beginIndex);
+            final var number = remaining.substring(beginIndex).replace(" ", "");
             for (var i = 0; i <= 9; i++) offset.suggest(number + i);
             builder.add(offset);
-            final var end = builder.createOffset(builder.getStart() + remaining.length());
-            builder.add(end.suggest(">"));
-        } else if (!trimmed.endsWith("//")) {
-            final var offset = builder.createOffset(builder.getStart() + remaining.length());
-            if (!trimmed.contains(",")) builder.add(offset.suggest(","));
+            final var end = builder.createOffset(builder.getStart() + beginIndex);
+            builder.add(end.suggest(number + ">"));
+        } else if (!stripped.endsWith("//")) {
+            final var offset = builder.createOffset(builder.getStart() + stripped.length());
+            if (!stripped.contains(",")) builder.add(offset.suggest(","));
             builder.add(offset.suggest(">"));
         }
         return builder.buildFuture();

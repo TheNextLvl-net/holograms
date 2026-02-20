d7534ab5e9f258f6cb11efbbeaee3a04ebdb3312
david <mail@nonswag.dev>
2026-02-20 19:11:07 +0100

    Add `@since` 0.6.0 javadoc tags to ClickType

--- api/src/main/java/net/thenextlvl/hologram/action/ClickType.java
+++ api/src/main/java/net/thenextlvl/hologram/action/ClickType.java
@@ -2,12 +2,16 @@ package net.thenextlvl.hologram.action;
 
 /**
  * Represents the type of interaction performed by a player.
+ *
+ * @since 0.6.0
  */
 public enum ClickType {
     /**
      * Represents a left-click interaction performed by a player.
      * <p>
      * This type of interaction occurs when a player attacks.
+     *
+     * @since 0.6.0
      */
     LEFT,
 
@@ -15,6 +19,8 @@ public enum ClickType {
      * Represents a right-click interaction performed by a player.
      * <p>
      * This type of interaction occurs when a player interacts.
+     *
+     * @since 0.6.0
      */
     RIGHT,
 
@@ -22,6 +28,8 @@ public enum ClickType {
      * Represents a shift-left-click interaction performed by a player.
      * <p>
      * This type of interaction occurs when a player attacks while sneaking.
+     *
+     * @since 0.6.0
      */
     SHIFT_LEFT,
 
@@ -29,6 +37,8 @@ public enum ClickType {
      * Represents a shift-right-click interaction performed by a player.
      * <p>
      * This type of interaction occurs when a player interacts while sneaking.
+     *
+     * @since 0.6.0
      */
     SHIFT_RIGHT;
 
@@ -36,6 +46,7 @@ public enum ClickType {
      * Checks if the interaction type is a left-click.
      *
      * @return whether this interaction represents a left-click
+     * @since 0.6.0
      */
     public boolean isLeftClick() {
         return equals(LEFT) || equals(SHIFT_LEFT);
@@ -45,6 +56,7 @@ public enum ClickType {
      * Checks if the interaction type is a right-click.
      *
      * @return whether this interaction represents a right-click
+     * @since 0.6.0
      */
     public boolean isRightClick() {
         return equals(RIGHT) || equals(SHIFT_RIGHT);
@@ -54,6 +66,7 @@ public enum ClickType {
      * Checks if the interaction type is a shift-click.
      *
      * @return whether this interaction represents either a shift-left-click or a shift-right-click
+     * @since 0.6.0
      */
     public boolean isShiftClick() {
         return equals(SHIFT_LEFT) || equals(SHIFT_RIGHT);

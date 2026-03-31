0dcacbd527f36cc31946d67eabfd69662f0d1bad
david <mail@nonswag.dev>
2026-03-31 22:08:40 +0200

    Update ServiceIO integration

---
+++ src/main/java/net/thenextlvl/hologram/service/ServicePagedHologramLine.java
@@ -0,0 +1,213 @@
+package net.thenextlvl.hologram.service;
+
+import net.thenextlvl.hologram.line.PagedHologramLine;
+import net.thenextlvl.service.api.capability.CapabilityException;
+import net.thenextlvl.service.api.hologram.line.BlockHologramLine;
+import net.thenextlvl.service.api.hologram.line.EntityHologramLine;
+import net.thenextlvl.service.api.hologram.line.HologramLine;
+import net.thenextlvl.service.api.hologram.line.ItemHologramLine;
+import net.thenextlvl.service.api.hologram.line.StaticHologramLine;
+import net.thenextlvl.service.api.hologram.line.TextHologramLine;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.jspecify.annotations.NullMarked;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+@NullMarked
+public final class ServicePagedHologramLine extends ServiceHologramLine<PagedHologramLine> implements net.thenextlvl.service.api.hologram.line.PagedHologramLine {
+    public ServicePagedHologramLine(final ServiceHologram hologram, final PagedHologramLine line) {
+        super(hologram, line);
+    }
+
+    @Override
+    public Stream<StaticHologramLine> getPages() {
+        return line.getPages().map(this::wrapPage);
+    }
+
+    private StaticHologramLine wrapPage(final net.thenextlvl.hologram.line.StaticHologramLine line) {
+        return switch (line) {
+            case final net.thenextlvl.hologram.line.ItemHologramLine l -> new ServiceItemHologramLine(hologram, l);
+            case final net.thenextlvl.hologram.line.BlockHologramLine l -> new ServiceBlockHologramLine(hologram, l);
+            case final net.thenextlvl.hologram.line.EntityHologramLine l -> new ServiceEntityHologramLine(hologram, l);
+            case final net.thenextlvl.hologram.line.TextHologramLine l -> new ServiceTextHologramLine(hologram, l);
+            default -> throw new IllegalStateException("Unexpected value: " + line);
+        };
+    }
+
+    @Override
+    public Optional<StaticHologramLine> getPage(final int index) {
+        return line.getPage(index).map(this::wrapPage);
+    }
+
+    @Override
+    public <T extends StaticHologramLine> Optional<T> getPage(final int index, final Class<T> type) {
+        return getPage(index).map(type::isInstance).map(type::cast);
+    }
+
+    @Override
+    public int getPageCount() {
+        return line.getPageCount();
+    }
+
+    @Override
+    public int getPageIndex(final HologramLine line) {
+        return line instanceof final ServiceHologramLine<?> service ? this.line.getPageIndex(service.line) : -1;
+    }
+
+    @Override
+    public TextHologramLine addTextPage() throws CapabilityException {
+        return new ServiceTextHologramLine(hologram, line.addTextPage());
+    }
+
+    @Override
+    public ItemHologramLine addItemPage() throws CapabilityException {
+        return new ServiceItemHologramLine(hologram, line.addItemPage());
+    }
+
+    @Override
+    public BlockHologramLine addBlockPage() throws CapabilityException {
+        return new ServiceBlockHologramLine(hologram, line.addBlockPage());
+    }
+
+    @Override
+    public EntityHologramLine addEntityPage(final EntityType entityType) throws IllegalArgumentException, CapabilityException {
+        return new ServiceEntityHologramLine(hologram, line.addEntityPage(entityType));
+    }
+
+    @Override
+    public boolean removePage(final int index) {
+        return line.removePage(index);
+    }
+
+    @Override
+    public boolean removePage(final HologramLine page) {
+        return page instanceof final ServiceHologramLine<?> service && line.removePage(service.line);
+    }
+
+    @Override
+    public boolean clearPages() {
+        return line.clearPages();
+    }
+
+    @Override
+    public boolean swapPages(final int first, final int second) {
+        return line.swapPages(first, second);
+    }
+
+    @Override
+    public boolean movePage(final int from, final int to) {
+        return line.movePage(from, to);
+    }
+
+    @Override
+    public TextHologramLine setTextPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceTextHologramLine(hologram, line.setTextPage(index));
+    }
+
+    @Override
+    public ItemHologramLine setItemPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceItemHologramLine(hologram, line.setItemPage(index));
+    }
+
+    @Override
+    public BlockHologramLine setBlockPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceBlockHologramLine(hologram, line.setBlockPage(index));
+    }
+
+    @Override
+    public EntityHologramLine setEntityPage(final int index, final EntityType entityType) throws IllegalArgumentException, IndexOutOfBoundsException, CapabilityException {
+        return new ServiceEntityHologramLine(hologram, line.setEntityPage(index, entityType));
+    }
+
+    @Override
+    public TextHologramLine insertTextPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceTextHologramLine(hologram, line.insertTextPage(index));
+    }
+
+    @Override
+    public ItemHologramLine insertItemPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceItemHologramLine(hologram, line.insertItemPage(index));
+    }
+
+    @Override
+    public BlockHologramLine insertBlockPage(final int index) throws IndexOutOfBoundsException, CapabilityException {
+        return new ServiceBlockHologramLine(hologram, line.insertBlockPage(index));
+    }
+
+    @Override
+    public EntityHologramLine insertEntityPage(final int index, final EntityType entityType) throws IllegalArgumentException, IndexOutOfBoundsException, CapabilityException {
+        return new ServiceEntityHologramLine(hologram, line.insertEntityPage(index, entityType));
+    }
+
+    @Override
+    public Duration getInterval() {
+        return line.getInterval();
+    }
+
+    @Override
+    public boolean setInterval(final Duration interval) throws IllegalArgumentException {
+        if (getInterval().equals(interval)) return false;
+        line.setInterval(interval);
+        return true;
+    }
+
+    @Override
+    public boolean isRandomOrder() {
+        return line.isRandomOrder();
+    }
+
+    @Override
+    public boolean setRandomOrder(final boolean random) {
+        if (isRandomOrder() == random) return false;
+        line.setRandomOrder(random);
+        return true;
+    }
+
+    @Override
+    public boolean isPaused() {
+        return line.isPaused();
+    }
+
+    @Override
+    public boolean setPaused(final boolean paused) {
+        if (isPaused() == paused) return false;
+        line.setPaused(paused);
+        return true;
+    }
+
+    @Override
+    public CompletableFuture<Boolean> cyclePage(final Player player) {
+        return line.cyclePage(player);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> cyclePage(final Player player, final int amount) {
+        return line.cyclePage(player, amount);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setPage(final Player player, final int page) throws IndexOutOfBoundsException {
+        return line.setPage(player, page);
+    }
+
+    @Override
+    public OptionalInt getCurrentPageIndex(final Player player) {
+        return line.getCurrentPageIndex(player);
+    }
+
+    @Override
+    public Optional<StaticHologramLine> getCurrentPage(final Player player) {
+        return line.getCurrentPage(player).map(this::wrapPage);
+    }
+
+    @Override
+    public void forEachPage(final Consumer<StaticHologramLine> action) {
+        line.getPages().map(this::wrapPage).forEach(action);
+    }
+}
--- src/main/java/net/thenextlvl/hologram/service/ServiceTextHologramLine.java
+++ src/main/java/net/thenextlvl/hologram/service/ServiceTextHologramLine.java
@@ -1,21 +1,108 @@
 package net.thenextlvl.hologram.service;
 
+import net.kyori.adventure.text.Component;
 import net.thenextlvl.hologram.line.TextHologramLine;
+import org.bukkit.Color;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TextDisplay;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Optional;
 
 @NullMarked
-public final class ServiceTextHologramLine extends ServiceDisplayHologramLine<String, TextHologramLine> {
-    public ServiceTextHologramLine(final TextHologramLine line) {
-        super(line);
+public final class ServiceTextHologramLine extends ServiceDisplayHologramLine<TextHologramLine> implements net.thenextlvl.service.api.hologram.line.TextHologramLine {
+    public ServiceTextHologramLine(final ServiceHologram hologram, final TextHologramLine line) {
+        super(hologram, line);
     }
 
     @Override
-    public String getContent() {
-        return line.getUnparsedText().orElse("");
+    public Optional<Component> getText(final Player player) {
+        return line.getText(player);
     }
 
     @Override
-    public void setContent(final String content) {
-        line.setUnparsedText(content);
+    public Optional<String> getUnparsedText() {
+        return line.getUnparsedText();
+    }
+
+    @Override
+    public boolean setText(@Nullable final Component text) {
+        return line.setText(text);
+    }
+
+    @Override
+    public boolean setUnparsedText(@Nullable final String text) {
+        return line.setUnparsedText(text);
+    }
+
+    @Override
+    public int getLineWidth() {
+        return line.getLineWidth();
+    }
+
+    @Override
+    public boolean setLineWidth(final int width) {
+        return line.setLineWidth(width);
+    }
+
+    @Override
+    public Optional<Color> getBackgroundColor() {
+        return line.getBackgroundColor();
+    }
+
+    @Override
+    public boolean setBackgroundColor(@Nullable final Color color) {
+        return line.setBackgroundColor(color);
+    }
+
+    @Override
+    public int getTextOpacity() {
+        return line.getTextOpacity();
+    }
+
+    @Override
+    public boolean setTextOpacity(final int opacity) {
+        return line.setTextOpacity(opacity);
+    }
+
+    @Override
+    public boolean isShadowed() {
+        return line.isShadowed();
+    }
+
+    @Override
+    public boolean setShadowed(final boolean shadow) {
+        return line.setShadowed(shadow);
+    }
+
+    @Override
+    public boolean isSeeThrough() {
+        return line.isSeeThrough();
+    }
+
+    @Override
+    public boolean setSeeThrough(final boolean seeThrough) {
+        return line.setSeeThrough(seeThrough);
+    }
+
+    @Override
+    public boolean isDefaultBackground() {
+        return line.isDefaultBackground();
+    }
+
+    @Override
+    public boolean setDefaultBackground(final boolean defaultBackground) {
+        return line.setDefaultBackground(defaultBackground);
+    }
+
+    @Override
+    public TextDisplay.TextAlignment getAlignment() {
+        return line.getAlignment();
+    }
+
+    @Override
+    public boolean setAlignment(final TextDisplay.TextAlignment alignment) {
+        return line.setAlignment(alignment);
     }
 }
